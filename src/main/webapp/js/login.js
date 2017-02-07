3e3ea761658fd9ea5754d1f2b11361e72e6e4558
Major Miao <majorm@ehi.ehealth.com>
2017-02-07 11:06:04 +0800

    Signed-off-by: Major Miao <majorm@ehi.ehealth.com>

--- src/main/webapp/js/login.js
+++ src/main/webapp/js/login.js
@@ -9,7 +9,7 @@ var app = angular.module('myApp', []);
                 data:{username:$scope.username,password:$scope.password}
                 //dataType:'json'
             }).success(function(data){
-                if(data == "12345")
+                if(data == "12345bbb")
                 {
                 	 alert("登录成功");
                 }
